d250ac7b2200cb40a40f28a955936e645785bce1
luizcanf <luizcanf@gmail.com>
2023-09-27 23:15:15 -0300

    Implementação da rota de login

--- server-ppdm.js
+++ server-ppdm.js
@@ -19,14 +19,14 @@ app.post('/cadastraUsuario', (req, res) => {
     const registro = JSON.stringify(usuario)
     const nomeArquivo = `${pasta}/${usuario.email}.json`
     fs.writeFileSync(nomeArquivo, registro)
-    res.send({msg: "Usuário cadastrado com sucesso."});
+    res.send({ msg: "Usuário cadastrado com sucesso." });
 })
 
 app.get('/lista', (req, res) => {
     fs.readdir(__dirname, (err, files) => {
         if (err) {
             console.log(err);
-            res.send({erro: true, msg: "Erro ao acessar a lista de usuários."});
+            res.send({ erro: true, msg: "Erro ao acessar a lista de usuários." });
         } else {
             var registros = files.filter(data => data.includes('.json'))
             res.send({ usuarios: registros })
@@ -37,7 +37,27 @@ app.get('/lista', (req, res) => {
 app.get('/delete/:email', (req, res) => {
     var usuario = req.params.email
     fs.unlinkSync(`${usuario}.json`)
-    res.send({msg: "Usuário cadastrado com sucesso."})
+    res.send({ msg: "Usuário cadastrado com sucesso." })
+})
+
+app.post('/login', (req, res) => {
+    const email = req.body.email
+    const senha = req.body.senha
+    let usuario
+    let files = fs.readdirSync(pasta, { withFileTypes: true })
+    files.forEach(file => {
+        let dados = fs.readFileSync(`${pasta}/${file}`)
+        let conferencia = JSON.parse(dados)
+        if (conferencia.email == email && conferencia.senha == senha) {
+            usuario = {...conferencia}
+            console.log(`Login de ${usuario.nome} realizado com sucesso.`);
+        }
+    })
+    if (usuario) {
+        res.send(usuario)
+    } else {
+        res.send({ encontrado: false, msg: 'Usuário/senha incorretos.' })
+    }
 })
 
 port = 8080
